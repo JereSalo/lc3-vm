64a00eff7dc622a2f6855a844e91ffb3b5bbb3c0
JereSalo <jeresalo17@gmail.com>
2024-09-17 15:05:17 -0300

    add implementations for br and jmp

--- src/instructions/br.rs
+++ src/instructions/br.rs
@@ -1,7 +1,56 @@
-use crate::vm::VM;
+use crate::{condition_flag::ConditionFlag, vm::VM};
+
+use super::sign_extend;
 
 impl VM {
     pub fn op_br(&mut self, instr: u16) {
+        // It is a conditional jump based on flags N Z and P.
+        let n = ((instr >> 11) & 0x1) == 1;
+        let z = ((instr >> 10) & 0x1) == 1;
+        let p = ((instr >> 9) & 0x1) == 1;
+
+        let pc_offset = instr & 0x1FF;
+
         
+        // This could be more pretty
+        if n && self.reg.cond == ConditionFlag::Neg || z && self.reg.cond == ConditionFlag::Zro || p && self.reg.cond ==ConditionFlag::Pos {
+            self.reg.pc += sign_extend(pc_offset, 9);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_op_br_pos_true() {
+        // instr: 0000 001 000001010 -> This is BRp with 10 offset.
+        let mut vm = VM::new();
+        vm.reg.cond = ConditionFlag::Pos; // Condition flag set to Pos
+        vm.reg.pc = 0x3000;
+
+        let instr = 0b0000_001_000001010; // BRp with offset 10
+        vm.op_br(instr);
+
+        let expected_pc = 0x300A;
+
+        assert_eq!(expected_pc, vm.reg.pc);
+    }
+
+    #[test]
+    fn test_op_br_pos_false() {
+        // instr: 0000 001 000001010 -> This is BRp with 10 offset.
+        let mut vm = VM::new();
+        vm.reg.cond = ConditionFlag::Neg; // Condition flag set to Neg!
+        vm.reg.pc = 0x3000;
+        let expected_pc = vm.reg.pc; // Shouldn't change the PC after this operation.
+
+        let instr = 0b0000_001_000001010; // BRp with offset 10
+        vm.op_br(instr);
+
+        
+
+        assert_eq!(expected_pc, vm.reg.pc);
     }
 }
--- src/instructions/jmp.rs
+++ src/instructions/jmp.rs
@@ -1,5 +1,30 @@
 use crate::vm::VM;
 
 impl VM {
-    pub fn op_jmp(&mut self, instr: u16) {}
+    pub fn op_jmp(&mut self, instr: u16) {
+        let r: usize = ((instr >> 6) & 0b111).into();
+
+        self.reg.pc = self.reg.general[r];
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn jump(){
+        let mut vm = VM::new();
+        vm.reg.pc = 0x3000;
+        let r = 3;
+        let expected_pc = 0x777;
+        vm.reg.update(r, expected_pc); // Store expected program count on register.
+
+        let instr = 0b1100_000_011_000000; // JMP to location in register 3 (011)
+
+        vm.op_jmp(instr);
+
+        assert_eq!(vm.reg.pc, expected_pc);
+    }
+
 }
