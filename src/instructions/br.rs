385f395addbfad8e3d12d01be26be8ca8b106b9d
JereSalo <jeresalo17@gmail.com>
2024-09-17 14:24:09 -0300

    fix opcodes order in enum

--- src/instructions/br.rs
+++ src/instructions/br.rs
@@ -1,5 +1,7 @@
 use crate::vm::VM;
 
 impl VM {
-    pub fn op_br(&mut self, instr: u16) {}
+    pub fn op_br(&mut self, instr: u16) {
+        
+    }
 }
--- src/opcode.rs
+++ src/opcode.rs
@@ -1,20 +1,20 @@
 pub enum Opcode {
-    OpAdd,
-    OpAnd,
-    OpNot,
-    OpBr,
-    OpJmp,
-    OpJsr,
-    OpLd,
-    OpLdi,
-    OpLdr,
-    OpLea,
-    OpSt,
-    OpSti,
-    OpStr,
-    OpTrap,
-    OpRes,
-    OpRti,
+    OpBr,   // 0
+    OpAdd,  // 1
+    OpLd,   // 2
+    OpSt,   // 3
+    OpJsr,  // 4
+    OpAnd,  // 5
+    OpLdr,  // 6
+    OpStr,  // 7
+    OpRti,  // 8
+    OpNot,  // 9
+    OpLdi,  // 10
+    OpSti,  // 11
+    OpJmp,  // 12
+    OpRes,  // 13
+    OpLea,  // 14
+    OpTrap, // 15
 }
 
 /// Convert u16 into Opcode
@@ -23,22 +23,22 @@ impl TryFrom<u16> for Opcode {
 
     fn try_from(value: u16) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(Opcode::OpAdd),
-            1 => Ok(Opcode::OpAnd),
-            2 => Ok(Opcode::OpNot),
-            3 => Ok(Opcode::OpBr),
-            4 => Ok(Opcode::OpJmp),
-            5 => Ok(Opcode::OpJsr),
-            6 => Ok(Opcode::OpLd),
-            7 => Ok(Opcode::OpLdi),
-            8 => Ok(Opcode::OpLdr),
-            9 => Ok(Opcode::OpLea),
-            10 => Ok(Opcode::OpSt),
+            0 => Ok(Opcode::OpBr),
+            1 => Ok(Opcode::OpAdd),
+            2 => Ok(Opcode::OpLd),
+            3 => Ok(Opcode::OpSt),
+            4 => Ok(Opcode::OpJsr),
+            5 => Ok(Opcode::OpAnd),
+            6 => Ok(Opcode::OpLdr),
+            7 => Ok(Opcode::OpStr),
+            8 => Ok(Opcode::OpRti),
+            9 => Ok(Opcode::OpNot),
+            10 => Ok(Opcode::OpLdi),
             11 => Ok(Opcode::OpSti),
-            12 => Ok(Opcode::OpStr),
-            13 => Ok(Opcode::OpTrap),
-            14 => Ok(Opcode::OpRes),
-            15 => Ok(Opcode::OpRti),
+            12 => Ok(Opcode::OpJmp),
+            13 => Ok(Opcode::OpRes),
+            14 => Ok(Opcode::OpLea),
+            15 => Ok(Opcode::OpTrap),
             _ => Err(()),
         }
     }
