8de4c756da171166546beedcd8428f254ffb027e
JereSalo <jeresalo17@gmail.com>
2024-09-18 18:22:18 -0300

    handle errors in memory

--- src/hardware/memory.rs
+++ src/hardware/memory.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{self, Read};
 
 const MEMORY_MAX: usize = 1 << 16;
 
@@ -28,21 +28,25 @@ impl Memory {
 
     pub fn read(&mut self, address: usize) -> u16 {
         if address == MemoryMappedRegister::MrKbsr as usize {
-            self.handle_keyboard();
+            if let Err(e) = self.handle_keyboard() {
+                eprintln!("Critical Error: Unable to handle keyboard input: {}", e);
+                std::process::exit(1); // Terminate program
+            }
         }
 
         self.memory[address]
     }
 
-    fn handle_keyboard(&mut self){
+    fn handle_keyboard(&mut self) -> io::Result<()> {
         let mut buffer = [0; 1];
-        std::io::stdin().read_exact(&mut buffer).unwrap();
+        std::io::stdin().read_exact(&mut buffer)?;
         if buffer[0] != 0 {
             self.write(MemoryMappedRegister::MrKbsr as usize, 1 << 15);
             self.write(MemoryMappedRegister::MrKbdr as usize, buffer[0] as u16);
         } else {
             self.write(MemoryMappedRegister::MrKbsr as usize, 0)
         }
+        Ok(())
     }
 
     pub fn write(&mut self, address: usize, value: u16) {
