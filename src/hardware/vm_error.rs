7aa37a5bb67112f0dbf9e77cb0936e8253cd091e
JereSalo <jeresalo17@gmail.com>
2024-09-20 17:53:34 -0300

    run cargo fmt

--- src/hardware/vm_error.rs
+++ src/hardware/vm_error.rs
@@ -11,7 +11,7 @@ pub enum VmError {
     InvalidTrapCode(u16),
     InvalidArguments,
     InvalidRegister(u16),
-    PcOverflow
+    PcOverflow,
 }
 
 // Implementing `Display` for `VmError`
@@ -21,13 +21,19 @@ impl fmt::Display for VmError {
             VmError::Io(err) => write!(f, "I/O Error: {}", err),
             VmError::InvalidOpcode(value) => write!(f, "Invalid Opcode encountered: {}", value),
             VmError::BadOpcode(opcode) => write!(f, "Bad Opcode encountered: {}", opcode),
-            VmError::InvalidTrapCode(value) => write!(f, "Invalid Trap Code encountered: {}", value),
+            VmError::InvalidTrapCode(value) => {
+                write!(f, "Invalid Trap Code encountered: {}", value)
+            }
             VmError::ReadImage(msg) => write!(f, "Error reading image file: {}", msg),
             VmError::InvalidArguments => {
                 write!(f, "Invalid arguments: Use 'cargo run <image_file> ..'")
             }
             VmError::InvalidRegister(reg) => {
-                write!(f, "Register out of bounds! Valid registers are 0 to 7. Actual value: {}", reg)
+                write!(
+                    f,
+                    "Register out of bounds! Valid registers are 0 to 7. Actual value: {}",
+                    reg
+                )
             } // This would happen if programmer made a mistake
             VmError::PcOverflow => {
                 write!(f, "Overflow of Program Counter has ocurred!")
